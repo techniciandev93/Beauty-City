d4239c4df930bd3958f7a7e4116c5f8736d45fbb
Weffy61 <weffy@mail.ru>
2024-01-25 18:26:26 +0300

    Remove SmsVerificationCode from Saloon app

--- BeautySaloon/models.py
+++ BeautySaloon/models.py
@@ -1,26 +1,6 @@
-from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
+from users.models import CustomUser
+from django.core.validators import MinValueValidator
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
-
-
-class SmsVerificationCode(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', blank=True, null=True)
-    code = models.IntegerField(verbose_name='Код подтверждения',
-                               validators=[
-                                   MaxValueValidator(9999, message='Код должен быть 4-значным числом'),
-                                   MinValueValidator(1000, message='Код должен быть 4-значным числом'),
-                               ]
-                               )
-    phone_number = PhoneNumberField(verbose_name='Мобильный номер')
-    date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления кода')
-
-    class Meta:
-        verbose_name = 'Sms-код для подтверждения'
-        verbose_name_plural = 'Sms-коды для подтверждения'
-
-    def __str__(self):
-        return f'{self.phone_number}'
 
 
 class Saloon(models.Model):
@@ -88,7 +68,7 @@ class Specialist(models.Model):
 
 
 class Order(models.Model):
-    client = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Клиент', related_name='orders')
+    client = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Клиент', related_name='orders')
     saloon = models.ForeignKey(Saloon, on_delete=models.CASCADE, verbose_name='Салон', related_name='orders')
     service = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name='Услуга', related_name='orders')
     specialist = models.ForeignKey('Specialist',
@@ -129,7 +109,7 @@ class Advertising(models.Model):
 
 
 class Review(models.Model):
-    client = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Клиент', related_name='reviews')
+    client = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Клиент', related_name='reviews')
     specialist = models.ForeignKey('Specialist',
                                    on_delete=models.SET_NULL,
                                    verbose_name='Специалист',

fe1467f5c0990cc86755d3f4e2a79bd4b6cbf1cc
Weffy61 <weffy@mail.ru>
2024-01-26 01:14:36 +0300

    Render index.html

--- BeautySaloon/admin.py
+++ BeautySaloon/admin.py
@@ -1,1 +1,26 @@
 from django.contrib import admin
+
+from BeautySaloon.models import Saloon, Service, Specialist, Review
+
+
+@admin.register(Saloon)
+class SaloonAdmin(admin.ModelAdmin):
+    fields = ['name', 'address', 'service', 'image']
+    raw_id_fields = ['service']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    fields = ['name', 'price', 'image']
+
+
+@admin.register(Specialist)
+class SpecialistAdmin(admin.ModelAdmin):
+    fields = ['saloon', 'name', 'career_start', 'speciality', 'start_work_time', 'end_work_time', 'service', 'image']
+    raw_id_fields = ['service', 'saloon']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ['client', 'specialist', 'text', 'rating', 'date']
+    raw_id_fields = ['client', 'specialist']
--- BeautySaloon/views.py
+++ BeautySaloon/views.py
@@ -1,11 +1,51 @@
 from django.shortcuts import render
-
-from users.forms import VerificationCodeForm
+from .models import Saloon, Service, Specialist, Review
 
 
 def index(request):
-    form = VerificationCodeForm()
-    return render(request, 'BeautySaloon/index.html', {'form': form})
+    saloons = [
+        {
+            'name': saloon.name,
+            'address': saloon.address,
+            'image': request.build_absolute_uri(saloon.image.url),
+        }
+        for saloon in Saloon.objects.all()
+    ]
 
+    services = [
+        {
+            'name': service.name,
+            'price': service.price,
+            'image': request.build_absolute_uri(service.image.url),
+        } for service in Service.objects.all()
+    ]
 
+    specialists = [
+        {
+            'name': specialist.name,
+            'speciality': specialist.speciality,
+            'experience': specialist.experience,
+            'review': specialist.reviews.count(),
+            'image': request.build_absolute_uri(specialist.image.url),
+        } for specialist in Specialist.objects.all()
+    ]
 
+    reviews = [
+        {
+            'client': review.client.phone_number,
+            'rating': review.rating,
+            'text': review.text,
+            'date': review.date
+        } for review in Review.objects.all()
+    ]
+
+    return render(
+        request,
+        'BeautySaloon/index.html',
+        context={
+            'saloons': saloons,
+            'services': services,
+            'specialists': specialists,
+            'reviews': reviews
+        }
+    )

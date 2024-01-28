9e0450ed459715745a1eb4c28d0eb53c00909dc3
Weffy61 <weffy@mail.ru>
2024-01-28 14:01:14 +0300

    Remove empty line

--- BeautySaloon/urls.py
+++ BeautySaloon/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('select_saloon/', views.get_saloons, name='saloon'),
     path('select_service/', views.get_services, name='select_services'),
     path('select_master/', views.get_masters, name='select_master'),
@@ -13,5 +12,4 @@ urlpatterns = [
     path('create_order/', views.create_order, name='create_order'),
     path('profile/', views.profile, name='profile'),
     path('review/', views.profile, name='review'),
-
 ]
